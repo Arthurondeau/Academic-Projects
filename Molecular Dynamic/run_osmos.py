e99fc3655856d9ee6d004cad6a2a68b1e5cd935e
Arthurondeau <84767820+Arthurondeau@users.noreply.github.com>
2022-12-05 22:36:02 +0100

    Add files via upload

--- Molecular Dynamic/run_osmos.py
+++ Molecular Dynamic/run_osmos.py
@@ -4,9 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def main():
-    sample_time=0.5
+    sample_time=0.5 #Time of each iteration
     np.random.seed(10)  # set random numbers to be always the same
-    simulation = Simul(sample_time,sigma_m = 0.1, sigma_p=0.3 ,omega = 0.1, L=4,Nb=10,No=2) #  sigma particle radius # L box size
+    simulation = Simul(sample_time,sigma_m = 0.1, sigma_p=0.3 ,omega = 0.1, L=4,Nb=10,No=2) 
+    #  sigma_p particle radius # sigma_m particle membrane radius # L box size # omega distance between each membrane particle # Nb number of blue particle # No number of orange particle
     print(simulation.__doc__)  # print the documentation from the class
 
     animate = AnimateSimul(simulation)
